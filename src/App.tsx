06a5207d96533aa36776fba075053751a5e9c74c
Mongark <mongarkprogrammer@gmail.com>
2023-02-15 22:18:30 -0400

    Added state for Modal in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import {
+  useState
+} from 'react';
+
 import './App.css'
 import Modal from './Modal';
 
 function App() {
+  const [show, set_show] = useState(false);
+
   return (
     <div>
       <button
         style={{ margin: "10px" }}
+        onClick={() => set_show(true)}
         >
         Show Modal
       </button>
 
-      <Modal show={true}/>
+      <Modal show={show}/>
     </div>
   );
 }
